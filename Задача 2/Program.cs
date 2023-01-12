e2b8b86654f920ce30e305c805c04cb87d7ab756
homeworkdome <homeworkdome@outlook.com>
2023-01-12 11:48:21 +0300

    Добавьте файлы проекта.

---
+++ Задача 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+class Program
+    {
+        static void GetParamsCube(double l, out double s, out double v)
+        {
+            s = 6 * l * l;
+            v = l * l * l;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            double l = Convert.ToDouble(Console.ReadLine());
+            double s;
+            double v;
+            GetParamsCube(l, out s, out v);
+            Console.WriteLine("Площадь и объем куба будут равны:");
+            Console.WriteLine("{0:.00} {1}", s, v);
+        }
+    }
+}
